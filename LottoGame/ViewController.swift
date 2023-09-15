eb0ab7085be9b674717e0e651e73d3531db330a7
syyoo <yeol2s@naver.com>
2023-09-16 01:46:41 +0900

    NewProjectStart

---
+++ LottoGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LottoGame
+//
+//  Created by 유성열 on 2023/09/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
